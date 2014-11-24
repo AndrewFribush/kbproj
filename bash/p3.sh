2b9dedf8e90f94f114eb2eed25a6f14e6c8ab4a1
Andrew <andrewfribush@gmail.com>
2014-11-24 09:49:07 -0500

    Edited markdown, problems through four started.

---
+++ bash/p3.sh
@@ -0,0 +1,4 @@
+largestPrimeFactor(){
+	$1
+}
+largestPrimeFactor 600851475143
---
+++ python/p4.py
@@ -0,0 +1,8 @@
+def Palindrome(num):
+	if num == num[::-1]:
+		return true
+	else:
+		return false
+
+def name():
+	for i in
