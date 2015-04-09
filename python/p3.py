e7aba6a52059251a3d383717e113c5f1b2da5761
Andrew <andrewfribush@gmail.com>
2015-04-08 20:29:32 -0400

    P3 working well now

--- python/p3.py
+++ python/p3.py
@@ -4,10 +4,31 @@
 #functions but is extremely slow at such a large number
 import math
 
-isPrime=lambda x: all(x % i != 0 for i in range(int(x**0.5)+1)[2:])
+givenNum = 600851475143
+
+def isPrime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True
+
+def factors(num):    
+    return reduce(list.__add__, ([i, num//i] for i in range(1, int(num**0.5) + 1) if num % i == 0))
 
 def primeFactors(num):
-	return filter(isPrime, range(num)[2:])[:5]
+	sortedFactors = sorted(factors(num))
+	finalList = []
+	for i in sortedFactors:
+		if isPrime(i):
+			finalList.append(i)
+	return finalList[len(finalList)-1]
 
-x = primeFactors(6008)
-print(x[len(x)-1])
+x = primeFactors(givenNum)
+print(x)
