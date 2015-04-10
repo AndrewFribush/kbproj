e787ec7538cbeed7e89c621b9e56a3717c7effc8
Andrew <andrewfribush@gmail.com>
2015-04-10 18:25:13 -0400

    P6 working

--- c++/p6.cpp
+++ c++/p6.cpp
@@ -1,6 +1,6 @@
-//The sum of the squares of the first ten natural numbers is, 12 + 22 + ... + 102 = 385
+//The sum of the squares of the first ten natural numbers is, 1**2 + 2**2 + ... + 10**2 = 385
 
-//The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)2 = 552 = 3025
+//The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)**2 = 552 = 3025
 
 //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
@@ -10,5 +10,15 @@
 
 int main()
 {
-	return void;
+	long sum = 0;
+	long squared = 0;
+	long result = 0;
+ 
+	const int N = 100;
+ 
+	sum = N * (N+1)/ 2;
+	squared = (N * (N + 1) * (2 * N + 1)) / 6;
+ 
+	result = sum * sum - squared;
+	return result;
 }
